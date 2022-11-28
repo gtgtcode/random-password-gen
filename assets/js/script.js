d31060b9b0bfb97bdbc93f6d0946ae4de1379429
GeorgeWorkPC <glthomas526@gmail.com>
2022-11-28 12:20:25 -0500

    added comments to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,37 @@
+//Gets the header.
 var headerContent = document.getElementById('header-content');
 
-let passwordGenerated = false;
-
-if (passwordGenerated == false) {
-    headerContent.innerHTML = "Generate a password!";
-    console.log("working");
-}
-
-let output = "";
+//Displays the header.
+headerContent.innerHTML = "Generate a password!";
 
 function generatePassword() {
     //Declaring Variables
 
     //This will be the output string.
-    
+    let output = "";
     //This is the character set used.
     let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
     //This is the length of the password. (Between 8-12).
     var pwLength = Math.ceil(7 + (Math.random() * 5));
-    console.log(pwLength);
 
+    /*
+        For loop for adding new characters to the output
+        variable as long as the length is under pwLength's.
+    */
     for (var i = 0; i < pwLength; i++) {
+        //Resets output when loop starts over.
         if (i == 0) {
             output = "";
         }
+
+        //Selects random character from charset.
         var ranCharset = Math.floor(Math.random() * charset.length); 
+
+        //Adds ranCharset to the output.
         output = output + charset.charAt(ranCharset);
-        console.log(output);
     }
 
+    //This displays the finished password on screen, only when the for loop has finished.
     if (i == pwLength) {
         document.getElementById("password").innerHTML = output;
     }
