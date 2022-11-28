0f9cc9008e0122101dea99ec01e4a7a2675183b5
GeorgeWorkPC <glthomas526@gmail.com>
2022-11-28 11:37:13 -0500

    declared variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,16 @@ if (passwordGenerated == false) {
     console.log("working");
 }
 
+function generatePassword() {
+    //Declaring Variables
 
+    //This will be the output string.
+    let output = '';
+    //This is the character set used.
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    //This is the length of the password. (Between 8-12).
+    var length = Math.ceil(8 + (Math.random() * 4));
+    console.log(length);
+
+}
 
