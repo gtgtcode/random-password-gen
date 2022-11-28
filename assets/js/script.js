9c5eaa7523de7caaa711dc71b2c50751976b7dde
GeorgeWorkPC <glthomas526@gmail.com>
2022-11-28 09:51:26 -0500

    added header

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var headerContent = document.getElementById('header-content');
+
+let passwordGenerated = false;
+
+if (passwordGenerated == false) {
+    headerContent.innerHTML = "Generate a password!";
+    console.log("working");
+}
+
